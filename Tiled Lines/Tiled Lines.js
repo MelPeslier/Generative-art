46a5c9dea1339c84bd8bb7ff96da88102140c15c
MelPeslier <mel.peslier@gmail.com>
2023-08-06 15:11:03 -0300

    Ajout Tiled Lines et Double Tiled Lines + Mise en page et choix dans un menu déroulant

--- Tiled Lines/Tiled Lines.js
+++ Tiled Lines/Tiled Lines.js
@@ -2,33 +2,70 @@
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 
-var step = 30;
-var size = window.innerWidth / 1.4;
-var dpr = window.devicePixelRatio;
-canvas.width = size * dpr;
-console.log(size);
-canvas.height = size * dpr;
-context.scale(dpr, dpr);
+var step = 40;
+// var widthSize = window.innerWidth;
+// var heightSize = window.innerHeight;
+// var dpr = window.devicePixelRatio;
+// canvas.width = widthSize;
+// canvas.height = heightSize;
+// context.scale(dpr, dpr);
 
 context.lineCap = 'square';
 context.lineWidth = 2;
 
-function draw(x, y, width, height){
+function draw(x, y, width, height, key){
 
    var leftToRight = Math.random() >= 0.5;
 
-   if (leftToRight) {
-      context.moveTo(x,y);
-      context.lineTo(x + width, y + height);
-   } else {
-      context.moveTo(x + width, y);
-      context.lineTo(x, y + height);
+   context.beginPath();
+   switch (key) {
+      case 'strait-line':
+         if (leftToRight) {
+            context.moveTo(x,y);
+            context.lineTo(x + width, y + height);
+         } else {
+            context.moveTo(x + width, y);
+            context.lineTo(x, y + height);
+         }
+         break;
+      
+      case 'double-strait-line':
+         if (leftToRight) {
+            context.moveTo(x + 1/2 * width,y);
+            context.lineTo(x + width, y + 1/2 * height);
+            context.moveTo(x,y + 1/2 * height);
+            context.lineTo(x + 1/2 * width, y + height);
+         } else {
+            context.moveTo(x + 1/2 * width,y);
+            context.lineTo(x, y + 1/2 * height);
+            context.moveTo(x + width,y + 1/2 * height);
+            context.lineTo(x + 1/2 * width, y + height);
+         }
+         break;
+         
+      default:
+         if (leftToRight) {
+            context.moveTo(x,y);
+            context.lineTo(x + width, y + height);
+         } else {
+            context.moveTo(x + width, y);
+            context.lineTo(x, y + height);
+         }
+         break;
    }
+   context.closePath();
    context.stroke();
 }
 
-for (let i = 0; i < size; i += step) {
-   for (let j = 0; j < size; j += step){
-      draw(i, j, step, step);
+function generate() {
+   selectElement = document.getElementById('art-select');
+   output = selectElement.value;
+
+   context.clearRect(0, 0, canvas.width, canvas.height);
+
+   for (let i = 0; i < canvas.width; i += step) {
+      for (let j = 0; j < canvas.height; j += step){
+         draw(i, j, step, step, output);
+      }
    }
 }
