a1dbf02e592ce505034ca78ec73c6769775fae79
MelPeslier <mel.peslier@gmail.com>
2023-08-06 15:53:46 -0300

    ajout des doubles quarts de cercle

--- Tiled Lines/Tiled Lines.js
+++ Tiled Lines/Tiled Lines.js
@@ -13,59 +13,92 @@ var step = 40;
 context.lineCap = 'square';
 context.lineWidth = 2;
 
-function draw(x, y, width, height, key){
+function draw(x, y, size, key){
 
    var leftToRight = Math.random() >= 0.5;
 
-   context.beginPath();
    switch (key) {
       case 'strait-line':
+         context.beginPath();
          if (leftToRight) {
             context.moveTo(x,y);
-            context.lineTo(x + width, y + height);
+            context.lineTo(x + size, y + size);
          } else {
-            context.moveTo(x + width, y);
-            context.lineTo(x, y + height);
+            context.moveTo(x + size, y);
+            context.lineTo(x, y + size);
          }
+         context.closePath();
+         context.stroke();
          break;
       
       case 'double-strait-line':
+         context.beginPath();
          if (leftToRight) {
-            context.moveTo(x + 1/2 * width,y);
-            context.lineTo(x + width, y + 1/2 * height);
-            context.moveTo(x,y + 1/2 * height);
-            context.lineTo(x + 1/2 * width, y + height);
+            context.moveTo(x + 1/2 * size,y);
+            context.lineTo(x + size, y + 1/2 * size);
+            context.moveTo(x,y + 1/2 * size);
+            context.lineTo(x + 1/2 * size, y + size);
          } else {
-            context.moveTo(x + 1/2 * width,y);
-            context.lineTo(x, y + 1/2 * height);
-            context.moveTo(x + width,y + 1/2 * height);
-            context.lineTo(x + 1/2 * width, y + height);
+            context.moveTo(x + 1/2 * size,y);
+            context.lineTo(x, y + 1/2 * size);
+            context.moveTo(x + size,y + 1/2 * size);
+            context.lineTo(x + 1/2 * size, y + size);
          }
+         context.closePath();
+         context.stroke();
+         break;
+
+      case 'double-quarter-circle':
+         if (leftToRight) {
+            context.moveTo(x + 1/2 * size, y + size);
+            context.beginPath();
+            context.arc(x, y + size, 1/2 * size, 0, Math.PI / 2);
+            context.stroke();
+
+            context.moveTo(x + 1/2 * size, y);
+            context.beginPath();
+            context.arc(x + size, y, 1/2 * size, Math.PI, (3 * Math.PI )/ 2);
+            context.stroke();
+         } else {
+            context.moveTo(x, y + 1/2 * size);
+            context.beginPath();
+            context.arc(x, y, 1/2 * size, (3 * Math.PI) / 2, 2 * Math.PI);
+            context.stroke();
+            
+            context.moveTo(x + size, y + 1/2 * size);
+            context.beginPath();
+            context.arc(x + size, y + size, 1/2 * size, Math.PI / 2, Math.PI);
+            context.stroke();
+         }
+         
          break;
          
       default:
          if (leftToRight) {
             context.moveTo(x,y);
-            context.lineTo(x + width, y + height);
+            context.lineTo(x + size, y + size);
          } else {
-            context.moveTo(x + width, y);
-            context.lineTo(x, y + height);
+            context.moveTo(x + size, y);
+            context.lineTo(x, y + size);
          }
+         context.closePath();
+         context.stroke();
          break;
    }
-   context.closePath();
-   context.stroke();
+
 }
 
 function generate() {
    selectElement = document.getElementById('art-select');
    output = selectElement.value;
 
+   console.log(output);
+
    context.clearRect(0, 0, canvas.width, canvas.height);
 
    for (let i = 0; i < canvas.width; i += step) {
       for (let j = 0; j < canvas.height; j += step){
-         draw(i, j, step, step, output);
+         draw(i, j, step, output);
       }
    }
 }
